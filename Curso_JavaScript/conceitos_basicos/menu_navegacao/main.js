2b123b1fa925bd0d60214928b84ea19952c0b657
Vando dos reis <vandodosreis2001@gmail.com>
2020-11-23 20:59:03 -0300

    Praticando mais formas de fazer o toggle

---
+++ Curso_JavaScript/conceitos_basicos/menu_navegacao/main.js
@@ -0,0 +1,28 @@
+function menuToggle() {
+
+  let = menuArea = document.querySelector('nav');
+
+  menuArea.classList.toggle('menu-opened')
+  
+  /*
+  
+  // primeira forma
+
+  if(menuArea.classList.contains('menu-opened') == true) {
+    menuArea.classList.remove('menu-opened');
+  } else {
+    menuArea.classList.add('menu-opened');
+  }
+
+
+  // Segunda forma de resolver 
+
+  if(menuArea.style.width == '200px') {
+    menuArea.style.width = '0px'
+  } else {
+    menuArea.style.width = '200px'
+  }
+  
+  */
+
+}
