095ba59a2b58aef8d2570a26e03dd0dbd5c5ad34
Vando dos reis <vandodosreis2001@gmail.com>
2020-12-07 18:54:59 -0300

    Includes e repeat

---
+++ Curso_JavaScript/manipulacao_array_ajax/includes_repeat/main.js
@@ -0,0 +1,8 @@
+const lista = [ 'Vandi', 'Vende', 'Vindi'];
+let nome = 'Vando'
+
+if (lista.includes(nome)) {
+  console.log('Nome disponivel'+'!!'.repeat(2))
+} else {
+  console.log('Not name undefined'+'!!'.repeat(2));
+}
