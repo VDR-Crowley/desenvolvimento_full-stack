f1122663d1132de4e86984ec720e975a4a2b7c8d
Vando dos reis <vandodosreis2001@gmail.com>
2020-12-10 21:32:37 -0300

    async and await

--- Curso_JavaScript/manipulacao_array_ajax/ajax_fetch/main.js
+++ Curso_JavaScript/manipulacao_array_ajax/ajax_fetch/main.js
@@ -1,14 +1,36 @@
-let url = 'https://alunos.b7web.com.br/api/ping';
-let params = {
-  method: 'POST',
-  body:JSON.stringify({
-    nome:'Vando',
-    sobrenome: 'Reis'
-  })
-};
+function requisitar() {
+  let url = 'https://alunos.b7web.com.br/api/ping';
+  let params = {
+    method: 'POST',
+    body:JSON.stringify({
+      nome:'Vando',
+      sobrenome: 'Reis'
+    })
+  };
 
-fetch(url, params)
-  .then((resposta) =>  resposta.json())
-  .then( (json) => {
-    console.log(json)
-  });
+  fetch(url, params)
+    .then((resposta) =>  resposta.json())
+    .then( (json) => {
+      console.log(json)
+    });
+}
+
+//requisitar();
+
+async function requisitar2() {
+  const url = 'https://alunos.b7web.com.br/api/ping';
+  const params = {
+    method: 'POST',
+    body:JSON.stringify({
+      nome:'Vando',
+      sobrenome: 'Reis'
+    })
+  };
+
+  const resposta = await fetch(url, params);
+  const json = await resposta.json();
+
+  console.log(json);
+}
+
+requisitar2();
