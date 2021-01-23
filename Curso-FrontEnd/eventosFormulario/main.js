740c197587cf228cdf4a6cbacce38e22135ca700
Vando dos reis <vandodosreis2001@gmail.com>
2021-01-23 00:45:37 -0300

    eventos de formulario

---
+++ Curso-FrontEnd/eventosFormulario/main.js
@@ -0,0 +1,11 @@
+function changeCity(element){
+  console.log("Cidade Mudada: "+element.value);
+}
+
+function isOnFocus() {
+  console.log("Campo está em foco!");
+}
+
+function exitTheFocus() {
+  console.log("Campo não está mais em foco!");
+}
