2281b3f8ddf691dc979502b437cb2888c05605c4
Vando <vandodosreis2001@gmail.com>
2021-07-11 22:51:04 -0300

    Praticando propriedade estaticas

--- php1/orientacao_objetos/oo-metodo-estatico/index.php
+++ php1/orientacao_objetos/oo-metodo-estatico/index.php
@@ -1,8 +1,10 @@
 <?php
 
   class Matematica {
+    public static string $name = "Vando"; 
+
     public static function soma($n1, $n2) {
       return $n1 + $n2;
     }
   }
-  echo "Soma: ".Matematica::soma(20, 20);
+  echo "Soma: ".Matematica::soma(20, 20)."calculo feito por ".Matematica::$name;
