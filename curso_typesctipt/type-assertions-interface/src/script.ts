e182eb1e2f697df4f72e6866bd743e14a532ca94
Vando <vandodosreis2001@gmail.com>
2021-11-09 23:46:39 -0300

    algumas mudanças nos tipos do type assertions interface

--- curso_typesctipt/type-assertions-interface/src/script.ts
+++ curso_typesctipt/type-assertions-interface/src/script.ts
@@ -6,12 +6,12 @@ let User: Pessoa = {
   nome: document.getElementById('nome') as HTMLInputElement,
   idade: document.getElementById('idade') as HTMLInputElement
 }
-let recarregar = document.getElementById('rec');
-let form = document.getElementById('form');
-function summitEvent(event) {
-  let resposta = document.getElementById('res');
+let recarregar = document.getElementById('rec') as HTMLElement;
+let form = document.getElementById('form') as HTMLFormElement;
+function summitEvent(event: Event) {
+  let resposta = document.getElementById('res') as HTMLElement;
   let nome: string = String(User.nome.value);
-  let idade: number;
+  let idade: number = 0;
   if(typeof User.idade != 'number' && typeof User.idade != 'string') idade = Number(User.idade.value);
   event.preventDefault();
   resposta.innerHTML = `<br>Nome: ${nome}<br>`;
